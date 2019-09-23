5e48814ecfbba401cdc0b5531a82a1924977cb58
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-09-23 16:23:37 +0200

    Fix indentation

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -10,38 +10,38 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Your Orders',
-          ),
+      appBar: AppBar(
+        title: const Text(
+          'Your Orders',
         ),
-        drawer: AppDrawer(),
-        body: FutureBuilder(
-          future: Provider.of<Orders>(context, listen: false).fetchOrders(),
-          builder: (ctx, dataSnapshot) {
-            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
               return Center(
-                child: CircularProgressIndicator(),
+                child: const Text(
+                  'An error occurred!',
+                ),
               );
             } else {
-              if (dataSnapshot.error != null) {
-                return Center(
-                  child: Text(
-                    'An error occurred!',
-                  ),
-                );
-              } else {
-                return Consumer<Orders>(
-                  builder: (ctx, orderData, child) => ListView.builder(
-                    itemCount: orderData.orders.length,
-                    itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-                  ),
-                );
-              }
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
             }
-          },
-        ));
+          }
+        },
+      ),
+    );
   }
 }
